7882e1433713ae2308a9866001d0a98b45af75a5
Gaowei Xu <gaowexu@amazon.com>
2022-01-04 20:35:29 +0800

    clean code

--- src/kitti_tools.py
+++ src/kitti_tools.py
@@ -12,18 +12,19 @@ class KITTITools(object):
     Reference:
     1. https://github.com/dtczhl/dtc-KITTI-For-Beginners/blob/master/python/object_viewer.py
     2. https://github.com/azureology/kitti-velo2cam
+    3. https://github.com/kuixu/kitti_object_vis
     """
-    def __init__(self, image_full_path, calib_full_path, label_full_path, cloud_points_full_path):
+    def __init__(self, image_full_path, calibration_full_path, label_full_path, cloud_points_full_path):
         """
         Constructor
 
         :param image_full_path: full path of image
-        :param calib_full_path: full path of calibration
+        :param calibration_full_path: full path of calibration file
         :param label_full_path: 3D object label file's full path
         :param cloud_points_full_path: cloud point full path, raw data is with form of [x0 y0 z0 r0 x1 y1 z1 r1 ...].
         """
         self._image_full_path = image_full_path
-        self._calib_full_path = calib_full_path
+        self._calibration_full_path = calibration_full_path
         self._label_full_path = label_full_path
         self._cloud_points_full_path = cloud_points_full_path
 
@@ -39,24 +40,29 @@ class KITTITools(object):
             'Person_sitting': [0, 0, 0.5]   # navy
         }
 
+        self._p2, self._r0_rect, self._tr_velodyne_to_camera = self.load_calibration_matrix()
+        self._gts_in_camera_coordinate_system = self.load_annotation()
+
+        # convert the ground truth from camera coordinate system to velodyne coordinate system
+        self._gts_in_velodyne_coordinate_system = self.convert_gt_from_camera_to_velodyne_coordinate_system()
+
     def load_calibration_matrix(self):
         """
-        load calibration matrix
+        load calibration matrix from calibration file
 
         P0 = gray_L (left gray camera)
         P1 = gray_R (right gray camera)
         P2 = rgb_L (left color camera)
         P3 = rgb_R (right color camera)
 
-        :return: p0, p1, p2, p3, r0_rect, tr_velo_to_cam, tr_imu_to_velo
+        :return: p2, r0_rect, tr_velodyne_to_camera
 
-        tr_velo_to_cam is the RT (rotation/translation) matrix from cloud point to image
-
-        For projecting a velodyne co-ordinate point into the camera-2 image:
-        [u v 1]^T = p2 * r0_rect * tr_velo_to_cam * [x y z 1]^T
-        where [u, v]^T is the image pixel index (column/row), and [x, y, z]^T is the cloud point coordinate
+        p2: projection matrix (after rectification) from a 3D coordinate in camera coordinate (x,y,z,1) to image
+            plane coordinate (u, v, 1)
+        r0_rect: rectifying rotation matrix of the reference camera, 4 x 4 matrix
+        tr_velodyne_to_camera: RT (rotation/translation) matrix from cloud point to image
         """
-        with open(self._calib_full_path) as rf:
+        with open(self._calibration_full_path) as rf:
             all_lines = rf.readlines()
 
         p0 = np.matrix([float(x) for x in all_lines[0].strip('\n').split()[1:]]).reshape(3, 4)
@@ -65,23 +71,23 @@ class KITTITools(object):
         p3 = np.matrix([float(x) for x in all_lines[3].strip('\n').split()[1:]]).reshape(3, 4)
 
         r0_rect = np.matrix([float(x) for x in all_lines[4].strip('\n').split()[1:]]).reshape(3, 3)
-        tr_velo_to_cam = np.matrix([float(x) for x in all_lines[5].strip('\n').split()[1:]]).reshape(3, 4)
-        tr_imu_to_velo = np.matrix([float(x) for x in all_lines[6].strip('\n').split()[1:]]).reshape(3, 4)
+        tr_velodyne_to_camera = np.matrix([float(x) for x in all_lines[5].strip('\n').split()[1:]]).reshape(3, 4)
+        tr_imu_to_velodyne = np.matrix([float(x) for x in all_lines[6].strip('\n').split()[1:]]).reshape(3, 4)
 
         # add a 1 in bottom-right, reshape r0_rect to 4 x 4
         r0_rect = np.insert(r0_rect, 3, values=[0, 0, 0], axis=0)
         r0_rect = np.insert(r0_rect, 3, values=[0, 0, 0, 1], axis=1)
 
-        # add a row in bottom of tr_velo_to_cam, reshape tr_velo_to_cam to 4 x 4
-        tr_velo_to_cam = np.insert(tr_velo_to_cam, 3, values=[0, 0, 0, 1], axis=0)
+        # add a row in bottom of tr_velodyne_to_camera, reshape tr_velodyne_to_camera to 4 x 4
+        tr_velodyne_to_camera = np.insert(tr_velodyne_to_camera, 3, values=[0, 0, 0, 1], axis=0)
 
-        return p0, p1, p2, p3, r0_rect, tr_velo_to_cam, tr_imu_to_velo
+        return p2, r0_rect, tr_velodyne_to_camera
 
     def load_annotation(self):
         """
         Load annotations of KITTI 3D object detection
 
-        :return: a list of all ground truth labels in current annotation file
+        :return: a list of all ground truth for various 3D objects in current annotation file
         """
         with open(self._label_full_path, 'r') as rf:
             all_lines = rf.readlines()
@@ -91,66 +97,61 @@ class KITTITools(object):
             line = line.strip()
             labels = line.split()
 
-            # 3D object's category information
-            category = labels[0]
+            # 3D object's category information, 'DontCare' labels denote regions in which objects have not been labeled,
+            # for example because they have been too far away from the laser scanner.
+            type = labels[0]
 
             # truncated Float from 0 (non-truncated) to 1 (truncated)
-            truncated_degree = float(labels[1])
+            truncation = float(labels[1])
 
             # occluded Integer (0,1,2,3) indicating occlusion state: 0 = fully visible,
             # 1 = partly occluded 2 = largely occluded, 3 = unknown
-            occluded_degree = float(labels[2])
+            occlusion = float(labels[2])
 
-            # alpha Observation angle of object, ranging [-pi..pi]
-            observation_angle = float(labels[3])
+            # alpha observation angle of object, ranging [-pi..pi]
+            alpha = float(labels[3])
 
-            # bbox 2D bounding box of object in the image (0-based index):
-            # contains left, top, right, bottom pixel coordinates
-            left_pixel = float(labels[4])
-            top_pixel = float(labels[5])
-            right_pixel = float(labels[6])
-            bottom_pixel = float(labels[7])
+            # 2D bounding box of object in the image: contains left, top, right, bottom pixel coordinates
+            x_min = float(labels[4])
+            y_min = float(labels[5])
+            x_max = float(labels[6])
+            y_max = float(labels[7])
 
-            # 3 dimensions 3D object dimensions: height, width, length (in meters)
+            # 3D object dimensions: height, width, length (in meters)
             height = float(labels[8])
             width = float(labels[9])
-            length = float(labels[9])
+            length = float(labels[10])
 
-            # 3 location 3D object location x,y,z in camera coordinates (in meters)
-            x = float(labels[10])
-            y = float(labels[11])
-            z = float(labels[12])
+            # 3D object location x,y,z in camera coordinates (in meters)
+            x = float(labels[11])
+            y = float(labels[12])
+            z = float(labels[13])
 
-            # rotation ry around Y-axis in camera coordinates system, ranging [-pi..pi]
-            rotation_y = float(labels[13])
+            # Rotation ry around Y-axis in camera coordinates [-pi..pi]
+            ry = float(labels[14])
 
             ground_truth.append(
                 {
-                    "category": category,
-                    "truncated_degree": truncated_degree,
-                    "occluded_degree": occluded_degree,
-                    "observation_angle": observation_angle,
-                    "image_bbox": [left_pixel, top_pixel, right_pixel, bottom_pixel],   # [x_min, y_min, x_max, y_max]
+                    "type": type,
+                    "truncation": truncation,
+                    "occlusion": occlusion,
+                    "alpha": alpha,
+                    "box2d": [x_min, y_min, x_max, y_max],
                     "height": height,
                     "width": width,
                     "length": length,
-                    "center_point": [x, y, z],
-                    "rotation": rotation_y
+                    "location": [x, y, z],
+                    "ry": ry
                 }
             )
 
         return ground_truth
 
-    @staticmethod
-    def project_velo_points_in_image(pts3d, p2, r0_rect, tr_velo_to_cam):
+    def project_velodyne_points_in_image(self, pts3d):
         """
-        project cloud points into plannar image
+        project cloud points into image plane
 
         :param pts3d: Nx4 numpy array, cloud points data
-        :param p2: projection matrix (after rectification) from a 3D coordinate in camera coordinate (x,y,z,1) to image
-                   plannar coordinate (u, v, 1)
-        :param r0_rect: rectifying rotation matrix of the reference camera, 4 x 4 matrix
-        :param tr_velo_to_cam: RT (rotation/translation) matrix from cloud point to image
         :return:
         """
         cloud_points = copy.deepcopy(pts3d)
@@ -166,7 +167,7 @@ class KITTITools(object):
         reflectances = pts3d[indices, 3]
 
         # [u v z]^T = p2 * r0_rect * tr_velo_to_cam * [x y z 1]^T
-        pts3d_cam = np.matmul(r0_rect, np.matmul(tr_velo_to_cam, cloud_points))     # x = right, y = down, z = forward
+        pts3d_cam = np.matmul(self._r0_rect, np.matmul(self._tr_velodyne_to_camera, cloud_points))
         pts3d_cam = np.array(pts3d_cam)
 
         # filter out points in front of camera
@@ -174,7 +175,7 @@ class KITTITools(object):
         pts3d_cam_in_front_of_camera = pts3d_cam[:, pts3d_cam_indices]
         pts3d_cam_in_front_of_camera = np.matrix(pts3d_cam_in_front_of_camera)
 
-        pts2d_cam = np.matmul(p2, pts3d_cam_in_front_of_camera)
+        pts2d_cam = np.matmul(self._p2, pts3d_cam_in_front_of_camera)
         pts2d_cam_normed = pts2d_cam
         pts2d_cam_normed[:2] = pts2d_cam[:2] / pts2d_cam[2, :]
         reflectances = reflectances[pts3d_cam_indices]
@@ -182,29 +183,69 @@ class KITTITools(object):
         assert pts2d_cam_normed.shape[1] == pts3d_cam_in_front_of_camera.shape[1] == reflectances.shape[0]
         return pts2d_cam_normed, pts3d_cam_in_front_of_camera, reflectances
 
-    def visualize_3d_object_in_points_cloud(self, ):
-        pass
+    def convert_gt_from_camera_to_velodyne_coordinate_system(self):
+        """
+        convert the ground truth frame camera coordinate system to velodyne coordinate system
+        - Camera: x = right, y = down, z = forward
+        - Velodyne: x = forward, y = left, z = up
 
-    def visualize_points_cloud_in_image(self, image_data, pc_data, p2, r0_rect, tr_velo_to_cam):
+        :return:
+        """
+        ret_gt_list = list()
+
+        for index, gt in enumerate(self._gts_in_camera_coordinate_system):
+            height, width, length = gt["height"], gt["width"], gt["length"]
+            [x_c, y_c, z_c] = gt["location"]
+            ry = gt["ry"]
+
+            # The reference point for the 3D bounding box for each object is centered on the bottom face of the box,
+            # as is shown in cs_overview.pdf. The corners of bounding box are computed as follows with
+            # respect to the reference point and in the object coordinate system.
+            x_corners = np.array([length/2, length/2, -length/2, -length/2, length/2, length/2, -length/2, -length/2])
+            y_corners = np.array([0, 0, 0, 0, -height, -height, -height, -height])
+            z_corners = np.array([width/2, -width/2, -width/2, width/2, width/2, -width/2, -width/2, width/2])
+            base_corners3d = np.array([x_corners, y_corners, z_corners])
+
+            # compute rotational matrix around yaw axis
+            rotation_matrix = np.array([
+                [np.cos(ry), 0.0, np.sin(ry)],
+                [0.0, 1.0, 0.0],
+                [-np.sin(ry), 0.0, np.cos(ry)]
+            ])
+
+            # corner3d in camera coordinate system
+            corners3d = np.dot(rotation_matrix, base_corners3d) + np.array([[x_c], [y_c], [z_c]])
+
+            corners3d_in_camera_coordinate_system = np.insert(corners3d, 3, np.ones(8, ), axis=0)
+            inverse_tr_velodyne_to_camera = np.linalg.inv(self._tr_velodyne_to_camera)
+            corners3d_in_velodyne_coordinate_system = np.dot(
+                inverse_tr_velodyne_to_camera,
+                corners3d_in_camera_coordinate_system)
+
+            corners3d_in_velodyne_coordinate_system = corners3d_in_velodyne_coordinate_system[0:3, :]
+
+            ret_gt_list.append(
+                {
+                    "type": gt["type"],
+                    "truncation": gt["truncation"],
+                    "occlusion": gt["occlusion"],
+                    "alpha": gt["alpha"],
+                    "box3d": corners3d_in_velodyne_coordinate_system.tolist()
+                }
+            )
+
+        return ret_gt_list
+
+    def plot_velodyne_points_in_image_plane(self, image_data, pc_data):
         """
         visualize points cloud in plannar image
 
         :param image_data: image data, RGB order
         :param pc_data: Nx4 numpy array, cloud points data
-        :param p2: projection matrix (after rectification) from a 3D coordinate in camera coordinate
-                   (x,y,z,1) to image plannar coordinate (u, v, 1), 3 x 4 matrix
-        :param r0_rect: rectifying rotation matrix of the reference camera, 4 x 4 matrix
-        :param tr_velo_to_cam: RT (rotation/translation) matrix from cloud point to image, 4 x 4 matrix
         :return:
         """
         height, width, channels = image_data.shape
-
-        pts2d_cam_normed, pts3d_cam_in_front_of_camera, reflectances = self.project_velo_points_in_image(
-            pts3d=pc_data,
-            p2=p2,
-            r0_rect=r0_rect,
-            tr_velo_to_cam=tr_velo_to_cam
-        )
+        pts2d_cam_normed, pts3d_cam_in_front_of_camera, reflectances = self.project_velodyne_points_in_image(pc_data)
 
         plt.figure(figsize=(12, 4), dpi=96, tight_layout=True)
         plt.imshow(image_data)
@@ -226,35 +267,61 @@ class KITTITools(object):
         plt.tight_layout()
         plt.show()
 
-    def visualize_2d_object_in_image(self, image_data, gts):
+    def plot_2d_box_in_image_plane(self, image_data, gts_in_camera_coordinate_system):
         """
-        visualize 2D object in image (frame) which is captured with left color camera
+        plot 2D object in image (frame) which is captured with left color camera
 
         :param image_data: RGB data of camera frame
-        :param gts: ground truth, which is a list
+        :param gts_in_camera_coordinate_system: ground truth in camera coordinate system
         :return: None
         """
         fig, ax = plt.subplots(figsize=(12, 4))
         ax.imshow(image_data)
 
-        for gt in gts:
-            [left_pixel, top_pixel, right_pixel, bottom_pixel] = gt["image_bbox"]
+        for gt in gts_in_camera_coordinate_system:
+            [left_pixel, top_pixel, right_pixel, bottom_pixel] = gt["box2d"]
             rect = patches.Rectangle(
-                (left_pixel, top_pixel), right_pixel-left_pixel, bottom_pixel-top_pixel,
-                linewidth=2, edgecolor=self._categories_color[gt["category"]], facecolor='none')
+                (left_pixel, top_pixel),
+                right_pixel-left_pixel,
+                bottom_pixel-top_pixel,
+                linewidth=1.5,
+                edgecolor=self._categories_color[gt["type"]],
+                facecolor='none')
             ax.add_patch(rect)
 
         plt.axis("off")
         fig.tight_layout()
         plt.show()
 
+    def plot_3d_box_in_image_plane(self, image_data, gts_in_velodyne_coordinate_system):
+        """
+        plot 3D object in image (frame) which is captured with left color camera
+
+        :param image_data: RGB data of camera frame
+        :param gts_in_velodyne_coordinate_system: ground truth in velodyne coordinate system
+        :return: None
+        """
+        pass
+
+    @staticmethod
+    def plot_3d_box_in_velodyne_coordinate_system(pc_data):
+        """
+        plot the 3D box of objects in velodyne coordinate system
+
+        :param pc_data: cloud points data, with shape (N, 4)
+        :return: None
+        """
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(pc_data[:, 0], pc_data[:, 1], pc_data[:, 2], s=0.1)
+        ax.grid(False)
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.set_zlabel('Z axis')
+        fig.tight_layout()
+        plt.show()
+
     def run(self):
-        # load calibration matrix
-        _, _, p2, _, r0_rect, tr_velo_to_cam, _ = self.load_calibration_matrix()
-
-        # load annotations
-        gts = self.load_annotation()
-
         # read camera data, shape = (375, 1242, 3)
         left_camera_image = cv2.imread(self._image_full_path, cv2.IMREAD_COLOR)         # BGR
         left_camera_image = cv2.cvtColor(left_camera_image, cv2.COLOR_BGR2RGB)          # RGB
@@ -262,34 +329,36 @@ class KITTITools(object):
         # read cloud point data
         pc_data = np.fromfile(self._cloud_points_full_path, dtype='<f4').reshape(-1, 4)
 
-        # self.visualize_2d_object_in_image(
-        #     image_data=left_camera_image,
-        #     gts=gts
-        # )
-
-        self.visualize_points_cloud_in_image(
+        # visualization
+        self.plot_velodyne_points_in_image_plane(
             image_data=left_camera_image,
-            pc_data=pc_data,
-            p2=p2,
-            r0_rect=r0_rect,
-            tr_velo_to_cam=tr_velo_to_cam
-        )
+            pc_data=pc_data)
 
-        # self.visualize_3d_object_in_points_cloud(
-        #     point_cloud_data=pc_data,
-        #     gts=gts,
-        #     p2=p2,
-        #     r0_rect=r0_rect,
-        #     tr_velo_to_cam=tr_velo_to_cam
-        # )
+        self.plot_2d_box_in_image_plane(
+            image_data=left_camera_image,
+            gts_in_camera_coordinate_system=self._gts_in_camera_coordinate_system)
+
+        self.plot_3d_box_in_image_plane(
+            image_data=left_camera_image,
+            gts_in_velodyne_coordinate_system=self._gts_in_velodyne_coordinate_system)
+
+        self.plot_3d_box_in_velodyne_coordinate_system(
+            pc_data=pc_data,
+            gts_in_velodyne_coordinate_system=self._gts_in_velodyne_coordinate_system,
+            enable_bird_eye_view=False)
+
+        self.plot_3d_box_in_velodyne_coordinate_system(
+            pc_data=pc_data,
+            gts_in_velodyne_coordinate_system=self._gts_in_velodyne_coordinate_system,
+            enable_bird_eye_view=True)
 
 
 if __name__ == "__main__":
     tools = KITTITools(
-        image_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/image_2/000011.png",
-        calib_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/calib/000011.txt",
-        label_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/label_2/000011.txt",
-        cloud_points_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/velodyne/000011.bin",
+        image_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/image_2/000021.png",
+        calibration_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/calib/000021.txt",
+        label_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/label_2/000021.txt",
+        cloud_points_full_path="../dataset/KITTI_3D_OBJECT_DETECTION_SAMPLED_DATASET/training/velodyne/000021.bin",
     )
 
     tools.run()
